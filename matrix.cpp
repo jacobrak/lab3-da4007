b9094d00ef228fb42631c3c66610d8b13d365a3b
jacobrak <155147415+jacobrak@users.noreply.github.com>
2025-09-24 13:14:21 +0200

    filename

--- matrix.cpp
+++ matrix.cpp
@@ -1,6 +1,24 @@
+#include <iostream>
+#include <fstream>
 using namespace std;
 
-class matrix{
-public:
-    
-};
+class Matrix{
+    public:
+        Matrix(const string& file_path){
+            read_file(file_path);
+        }
+        void read_file(const string& file_path){
+            ifstream file(file_path);
+
+            if (!file){
+                cerr << "bad filename " << file_path << endl;
+                return;
+            }
+
+        }
+};  
+
+int main(){
+    Matrix my_obj("m4.txt");
+    return 0;
+}
